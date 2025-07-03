62c8f7850c329809d018f985c68f6e866a4688cc
fafafafafafafafaa <vivi930319@gmail.com>
2025-07-03 09:57:06 +0800

    aaaa

---
+++ 0703/print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class print {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
+        sc.close(); // 建議加上關閉 Scanner
+    }
+}
+
