6e1b6c62f793c39c175b8a7dd5d14334a03f1744
fafafafafafafafaa <vivi930319@gmail.com>
2025-07-08 10:54:21 +0800

    add

--- 0703/fa2.java
+++ 0703/fa2.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class fa2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); // 使用者輸入 n，例如 8
-        int result = ((n + n) + (n * 5)) - 2;
-        System.out.println(result); // 輸出結果 54
+        int n = sc.nextInt();  // 讀入變數 n
+        int result = ((n + n) + (n * 5));  // 計算 ((n+n)+(n*5))
+        System.out.println(result);  // 輸出結果
         sc.close();
     }
 }
